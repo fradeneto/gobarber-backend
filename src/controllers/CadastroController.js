b328183c31557fdcda23b663667d18949aa23859
Orlando Frade Neto <fradeneto@Orlandos-MacBook-Pro.local>
2020-02-22 19:42:16 -0800

    feat: add sequelize

--- src/controllers/CadastroController.js
+++ src/controllers/CadastroController.js
@@ -2,8 +2,19 @@ import { Cadastro }  from '../models';
 
 class UserController {
   async store(req,res){
-    const cadastro = {teste: "ok"}
-    return res.status(200).json(cadastro)
+    const { ativo, nome, dia } = req.body;
+    await Cadastro.create({
+      ativo,
+      nome,
+      dia
+    })
+    .then(cadastro => {
+      return res.status(200).json(cadastro)
+    })
+    .catch(error => {
+      return res.status(200).json(error)
+    });
+    
   }
 }
 
